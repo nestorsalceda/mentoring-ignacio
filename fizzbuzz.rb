186d925326a2af72045ef105e604383f57485e27
Néstor Salceda <nestor.salceda@gmail.com>
2014-11-27 11:20:30 +0100

    Se añade la implementación de Buzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def FizzBuzz(number)
-  return "Fizz" if number != 5
-  "Buzz"
+  return "Buzz" if number % 5 == 0
+  "Fizz"
 end
 
 describe "Fizzbuzz" do
@@ -10,5 +10,13 @@ describe "Fizzbuzz" do
 
   it "returns Fizz if number is divisible by 3" do
     expect(FizzBuzz(6)).to eq("Fizz")
+  end
+
+  it "returns Buzz if number is 5" do
+    expect(FizzBuzz(5)).to eq("Buzz")
+  end
+
+  it "returns Buzz if number is divisible by 5" do
+    expect(FizzBuzz(10)).to eq("Buzz")
   end
 end
