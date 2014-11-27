da637cbde5be4eb9a108159441936766a75b953f
Néstor Salceda <nestor.salceda@gmail.com>
2014-11-27 11:44:02 +0100

    Se mueve la lógica a la clase Fixnum

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,40 +1,42 @@
-def isBuzz?(number)
-  number % 5 == 0
-end
+class Fixnum
+  def isBuzz?
+    self % 5 == 0
+  end
 
-def isFizz?(number)
-  number % 3 == 0
-end
+  def isFizz?
+    self % 3 == 0
+  end
 
-def FizzBuzz(number)
-  return "FizzBuzz" if isBuzz? number and isFizz? number
-  return "Buzz" if isBuzz? number
-  return "Fizz" if isFizz? number
-  number
+  def fizzBuzz
+    return "FizzBuzz" if isBuzz? and isFizz?
+    return "Buzz" if isBuzz?
+    return "Fizz" if isFizz?
+    self.to_s
+  end
 end
 
 describe "Fizzbuzz" do
   it "returns Fizz if number is 3" do
-    expect(FizzBuzz(3)).to eq("Fizz")
+    expect(3.fizzBuzz).to eq("Fizz")
   end
 
   it "returns Fizz if number is divisible by 3" do
-    expect(FizzBuzz(6)).to eq("Fizz")
+    expect(6.fizzBuzz).to eq("Fizz")
   end
 
   it "returns Buzz if number is 5" do
-    expect(FizzBuzz(5)).to eq("Buzz")
+    expect(5.fizzBuzz).to eq("Buzz")
   end
 
   it "returns Buzz if number is divisible by 5" do
-    expect(FizzBuzz(10)).to eq("Buzz")
+    expect(10.fizzBuzz).to eq("Buzz")
   end
 
   it "returns FizzBuzz if number is divisible by 3 and 5" do
-    expect(FizzBuzz(15)).to eq("FizzBuzz")
+    expect(15.fizzBuzz).to eq("FizzBuzz")
   end
 
   it "returns same number if is not Fizz or Buzz or FizzBuzz" do
-    expect(FizzBuzz(1)).to eq(1)
+    expect(1.fizzBuzz).to eq("1")
   end
 end
