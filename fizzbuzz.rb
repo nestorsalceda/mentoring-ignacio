d4a6710f745c4d9309b6ad9cf965612b4e7c2ac3
Néstor Salceda <nestor.salceda@gmail.com>
2014-11-27 11:30:10 +0100

    Se implementa el devolver un número si no se aplica ninguna regla anterior

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 def FizzBuzz(number)
   return "FizzBuzz" if number % 5 == 0 and number % 3 == 0
   return "Buzz" if number % 5 == 0
-  "Fizz"
+  return "Fizz" if number % 3 == 0
+  number
 end
 
 describe "Fizzbuzz" do
@@ -23,5 +24,9 @@ describe "Fizzbuzz" do
 
   it "returns FizzBuzz if number is divisible by 3 and 5" do
     expect(FizzBuzz(15)).to eq("FizzBuzz")
+  end
+
+  it "returns same number if is not Fizz or Buzz or FizzBuzz" do
+    expect(FizzBuzz(1)).to eq(1)
   end
 end
