030e2a9d0f28a3b59a519530eb33a8ce2693a65a
Néstor Salceda <nestor.salceda@gmail.com>
2014-12-11 12:39:46 +0100

    Se extraen los specs a otro fichero y se implementa la parte de aleatoriedad

--- rock-paper-scissors/game.rb
+++ rock-paper-scissors/game.rb
@@ -1,5 +1,4 @@
 class Game
-
   def initialize
     @winners = {}
     @winners[:scissors] = {}
@@ -19,62 +18,22 @@ class Game
   def play(hand1, hand2)
     return @winners[hand1][hand2]
   end
-
 end
 
-describe 'Rock Paper Scissors Game' do
-  before(:each) do
+class RandomGame
+  def initialize
     @game = Game.new
+    @choices = [:scissors, :rock, :paper]
   end
 
-  context 'when playing scissors' do
-    it 'wins against paper' do
-      winner = @game.play(:scissors, :paper)
-      expect(winner).to eq(:scissors)
-    end
+  def play(hand)
+    choice = @choices.sample
 
-    it 'loses against rock' do
-      winner = @game.play(:scissors, :rock)
-      expect(winner).to eq(:rock)
-    end
+    puts 'Playing: ', hand, choice
 
-    it 'ties against scissors' do
-      winner = @game.play(:scissors, :scissors)
-      expect(winner).to eq(:tie)
-    end
-  end
+    winner = @game.play(hand, choice)
 
-  context 'when playing rock' do
-    it 'wins against scissors' do
-      winner = @game.play(:rock, :scissors)
-      expect(winner).to eq(:rock)
-    end
-
-    it 'loses against paper' do
-      winner = @game.play(:rock, :paper)
-      expect(winner).to eq(:paper)
-    end
-
-    it 'ties against rock' do
-      winner = @game.play(:rock, :rock)
-      expect(winner).to eq(:tie)
-    end
-  end
-
-  context 'when playing paper' do
-    it 'loses against scissors' do
-      winner = @game.play(:paper, :scissors)
-      expect(winner).to eq(:scissors)
-    end
-
-    it 'ties against paper' do
-      winner = @game.play(:paper, :paper)
-      expect(winner).to eq(:tie)
-    end
-
-    it 'wins against rock' do
-      winner = @game.play(:paper, :rock)
-      expect(winner).to eq(:paper)
-    end
+    puts 'And winner is: ', winner
   end
 end
+
